5b958bf4ba7cdc5567faa1bbb74eb2ae4f8c82b6
Liguo Jiao <liguo@moov.cc>
2018-05-09 19:46:53 +1200

    update picker of edit page

--- MEGA-Test/Controller/CategoryPickerViewController.m
+++ MEGA-Test/Controller/CategoryPickerViewController.m
@@ -7,20 +7,42 @@
 //
 
 #import "CategoryPickerViewController.h"
+#import "KeepLayout.h"
+#import <Realm/Realm.h>
+#import "Category.h"
 
 @interface CategoryPickerViewController () <UIPickerViewDelegate, UIPickerViewDataSource>
 @property (nonatomic, strong) UIPickerView *categoryPicker;
+@property (nonatomic) RLMResults<Category *> *category;
 @end
 
 @implementation CategoryPickerViewController
 
+- (void)loadView
+{
+    [super loadView];
+    UIPickerView *picker = [[UIPickerView alloc]initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height / 2, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height / 2)];
+    picker.backgroundColor = [UIColor lightGrayColor];
+    picker.delegate = self;
+    picker.dataSource = self;
+    [self.view addSubview:picker];
+    self.categoryPicker = picker;
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismiss)];
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    [self.view addGestureRecognizer:tap];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    _categoryPicker = [[UIPickerView alloc]initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height/2, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height/2)];
-    _categoryPicker.backgroundColor = [UIColor blueColor];
-    [self.view addSubview:_categoryPicker];
+    self.category = [Category allObjects];
+    
+}
+
+- (void)dismiss
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (NSInteger)numberOfComponentsInPickerView:(nonnull UIPickerView *)pickerView
@@ -30,7 +52,16 @@
 
 - (NSInteger)pickerView:(nonnull UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
-    return 5;
+    return self.category.count;
 }
 
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return self.category[row].name;
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    
+}
 @end
